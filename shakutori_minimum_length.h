1c13a9187d8751db98a02641250c951ff995cb05
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2021-08-09 20:18:08 +0900

    Update shakutori_minimum_length.h

--- shakutori_minimum_length.h
+++ shakutori_minimum_length.h
@@ -1,18 +1,4 @@
 template<typename T>
-long long shakutori_count(vector<T> const &input, long long upper_limit) {
-  /* O(n) */
-  /* returns the number of sequences whose sum <= upper_limit. */
-  int r = 0, n = input.size();
-  long long sum = 0, count = 0;
-  for(int l=0; l<n; ++l) { // consider [l,r)
-    while(r<n && (sum + input[r] <= upper_limit)) sum += input[r++];
-    count += r - l;
-    sum -= input[l];
-  }
-  return count;
-}
-
-template<typename T>
 int shakutori_minimum_length(vector<T> const &input, long long lower_limit) {
   /* O(n) */
   /* returns the minimum length of sequence whose sum >= lower_limit. */
