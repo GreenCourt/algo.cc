45727841f61ef718dfccce14f34c518a2376398c
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2021-08-23 22:34:25 +0900

    Update imos.h

--- imos.h
+++ imos.h
@@ -1,11 +1,11 @@
 vector<int> Imos(int mx, vector<pair<int,int>> const &area) {
   /* O(mx + area.size()) */
-  vector<int> d(mx+2), sum(mx+1);
+  vector<int> d(mx+1);
   for(auto [l,r] : area) {
     assert(0<=l && l < r && r <=mx+1); /* [l,r) */
-    d[l]++, d[r]--;
+    d[l]++;
+    if(r <= mx) d[r]--;
   }
-  sum[0] = d[0];
-  for(int i=1; i<=mx; ++i) sum[i] = sum[i-1] + d[i];
-  return sum;
+  for(int i=1; i<=mx; ++i) d[i] += d[i-1];
+  return d;
 }
