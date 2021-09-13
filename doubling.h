3d6bec96255dd9358002f6b268fd105effe9e798
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2021-09-13 22:55:22 +0900

    Create doubling.h

---
+++ doubling.h
@@ -0,0 +1,22 @@
+struct Doubling {
+  vector<vector<int>> dbl;
+  Doubling(const vector<int> &single_move, long long k) { /* O(n log k) */
+    int n=single_move.size(), log2k = log2floor(k);
+    dbl.assign(log2k+1, vector<int>(n));
+    dbl[0] = vector(single_move);
+    for(int i=0; i<log2k; ++i) for(int x=0; x<n; ++x) dbl[i+1][x] = dbl[i][dbl[i][x]];
+  }
+  static int log2floor(unsigned long long k) { /* O(log k) */
+    int log2k = 0; for(unsigned long long K=k; K>1; K/=2) log2k++;
+    return log2k; // floor(log2(k)) if k>0 else 0
+  }
+  int query(int start, long long k) { /* O(log k) */
+    assert(k < (1LL<<dbl.size()));
+    int x = start;
+    while(k) {
+      int lk = log2floor(k);
+      x = dbl[lk][x], k -= 1LL<<lk;
+    }
+    return x;
+  }
+};
