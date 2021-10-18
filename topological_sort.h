45741ab55dfcaf4cb1b4fb9761c3a8f71e3f2234
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2021-10-18 20:47:38 +0900

    Create topological_sort.h

---
+++ topological_sort.h
@@ -0,0 +1,16 @@
+vector<int> topological_sort(const vector<vector<int>> &adjlist) {
+  /* O(|V| + |E|) */
+  int n = adjlist.size();
+  vector<int> degree_in(n);
+  for(int i=0; i<n; ++i) for(int u:adjlist[i]) degree_in[u]++;
+  vector<int> seq;
+  queue<int> q; // use priority_queue if you need a lexicographically smallest sequence in O(|V| log |V| + |E|)
+  for(int i=0; i<n; ++i) if(degree_in[i] == 0) q.push(i);
+  while(q.size()) {
+    int v = q.front(); q.pop();
+    seq.push_back(v);
+    for(int u: adjlist[v]) if(--degree_in[u] == 0) q.push(u);
+  }
+  if(seq.size() != n) return vector<int>(); // not a DAG
+  return seq;
+}
