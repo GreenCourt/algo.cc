448b4fa08a88a3e54eda4bb1f3b7f056c0f46525
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2021-10-31 23:57:27 +0900

    Update union_find.h

--- union_find.h
+++ union_find.h
@@ -4,14 +4,13 @@ struct UnionFind {
   UnionFind(int size=0) : data(size, -1), number_of_groups(size) { }
   bool unite(int x, int y) {
     x = root(x); y = root(y);
-    if (x != y) {
-      if (data[y] < data[x]) swap(x, y);
-      data[x] += data[y]; data[y] = x;
-    }
-    number_of_groups -= (x!=y);
-    return x != y;
+    if(x==y) return false;
+    if (data[y] < data[x]) swap(x, y);
+    data[x] += data[y]; data[y] = x;
+    number_of_groups--;
+    return true;
   }
-  bool find(int x, int y) { return root(x) == root(y); }
+  bool same(int x, int y) { return root(x) == root(y); }
   int root(int x) { return data[x] < 0 ? x : data[x] = root(data[x]); }
   int size(int x) { return -data[root(x)]; }
   vector<vector<int>> groups() {
