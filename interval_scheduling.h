f01799c4ae7287613b1aa8e2ba3ebc1ab97588bb
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2021-10-31 12:34:08 +0900

    Create interval_scheduling.h

---
+++ interval_scheduling.h
@@ -0,0 +1,14 @@
+template<typename T>
+int interval_scheduling(const vector<pair<T,T>> &RL, bool half_open=false) {
+  /* O(n log n) */
+  int n = RL.size();
+  if(n==0) return 0;
+  vector rl(RL);
+  sort(rl.begin(), rl.end()); 
+  int ans = 1;
+  T b = rl[0].first;
+  for(int i=1; i<n; ++i)
+    if(half_open ? (rl[i].second >= b) : (rl[i].second > b))
+      ans++, b=rl[i].first;
+  return ans;
+}
