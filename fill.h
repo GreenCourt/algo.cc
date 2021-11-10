a8fdfa1ca5263f4f11665c76527090d616b9656f
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2021-11-10 19:12:32 +0900

    Delete fill.h

--- fill.h
+++
@@ -1,4 +0,0 @@
-template<typename A,size_t N,typename T> void FILL(A (&array)[N],const T &val){fill((T*)array,(T*)(array+N),val);}
-template<typename T> void FILL(vector<T> &v, const T &x) {fill(v.begin(), v.end(), x);}
-template<typename T> void FILL(vector<vector<T>> &v, const T &x) {for(auto &i:v)fill(i.begin(), i.end(), x);}
-template<typename T> void FILL(vector<vector<vector<T>>> &v, const T &x) {for(auto &i:v) for(auto &j:i)fill(j.begin(), j.end(), x);}
