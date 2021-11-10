2aaca9ac8c412f72f09a6585fba143203b3ceada
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2021-11-10 19:11:24 +0900

    Delete bits.h

--- bits.h
+++
@@ -1,7 +0,0 @@
-inline unsigned long long extract_msb(unsigned long long x) { return 1LL << ((x==0) ? 0 : 64 - __builtin_clzll(x)-1); }
-inline unsigned long long extract_lsb(unsigned long long x) { return 1LL << (__builtin_ffsll(x)-1); }
-inline int msb_position(unsigned long long x) { return (x==0) ? 0 : 64 - __builtin_clzll(x); }
-inline int lsb_position(unsigned long long x) { return __builtin_ffsll(x); }
-
-inline int bit_rotate_right(int bits, int rot, int length) { return (bits>>rot) + ((((1<<rot)-1) & bits) << (length - rot)); }
-inline int bit_rotate_left(int bits, int rot, int length) { return ((bits<<rot)+(bits >> (length-rot))) & ((1<<length)-1); }
