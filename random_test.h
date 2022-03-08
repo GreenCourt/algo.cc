28509d12ba2f62f29ee65d7a3711dea3efe069d0
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2022-03-08 23:30:46 +0900

    Update and rename random_generator.h to random_test.h

--- random_test.h
+++ random_test.h
@@ -139,3 +139,16 @@ string random_string(int n, const string &population = string_digits + string_al
   for(int i=0; i<n; ++i) r.push_back(population[randint(0, sz-1)]);
   return r;
 }
+
+template<typename T>
+bool isclose(T a, T b, T rel_tol, T abs_tol) {
+  assert(0.0 <= rel_tol && rel_tol < 1.0);
+  assert(0.0 <= abs_tol && rel_tol < 1.0);
+  if (a == b) return true;
+  if (isinf(a) || isinf(b)) return false;
+  double diff = abs(b - a);
+  return (diff <= abs(rel_tol * b)) || (diff <= abs(rel_tol * a)) || (diff <= abs_tol);
+}
+
+template<typename T>
+inline bool isclose(T a, T b, T tolerance) { return isclose(a, b, tolerance, tolerance); }
