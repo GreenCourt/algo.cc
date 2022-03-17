7b6e9dab3c89cab24810b56dee95937d3a9a63fa
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2022-03-17 17:03:04 +0900

    Update modulo.h

--- modulo.h
+++ modulo.h
@@ -26,9 +26,9 @@ int modinv(long long a, int mod) {
   return (int)u;
 }
 
-int choose(int n, int k, int mod) {
+int choose(long long n, int k, int mod) {
   /* O(k log mod) */
-  assert(n>=0 && k>=0 && n < mod && k < mod);
+  assert(n>=0 && k>=0 && k < mod);
   long long c = 1;
   for(int i=1; i<=k; i++) c = (n-i+1) % mod * c % mod * modinv(i, mod) % mod; 
   return (int)c;
