49661203e4e8c59a304d5d5cde896e270180f802
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2022-05-02 07:40:07 +0900

    Update binary_search.h

--- binary_search.h
+++ binary_search.h
@@ -2,6 +2,7 @@ long long BinarySearch(long long l, long long r, function<bool(long long)> f) {
   /* O(log(r-l) * O(f)) */
   /* [l,r] both inclusive */
   /* Find the boundary T and F that satisfies (f(T)==true && f(F)==false && abs(T-F)==1). */
+  assert(l<r);
   bool fl = f(l), fr = f(r); assert(fl!=fr);
   long long &T = fl ? l : r, &F = fr ? l : r;
   while(r-l>1) {
