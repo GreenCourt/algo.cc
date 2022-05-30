18c58d907f6eca629bb199e934ab68a89b17f5b3
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2022-05-30 19:29:02 +0900

    Update rolling_hash.h

--- rolling_hash.h
+++ rolling_hash.h
@@ -1,13 +1,13 @@
 template<typename T>
 vector<int> find_all_matches_by_rolling_hash(const T& text, const T& pattern, unsigned long long b=1000000007) {
   /* O(n + m) */
-  /* overflow --> mod 2^64 */
   int n = pattern.size();
   int m = text.size();
   assert(n > 0);
   assert(m > 0);
   if(n > m) return vector<int>();
 
+  /* For unsigned long long, overflow is same as mod 2^64 */
   unsigned long long B = 1;
   for(int i=0; i<n-1; ++i) B *= b;
 
@@ -19,7 +19,6 @@ vector<int> find_all_matches_by_rolling_hash(const T& text, const T& pattern, un
 
   vector<int> index;
   if(hash_p == hash_t) index.push_back(0);
-
   for(int i=n; i<m; i++) {
     hash_t = (b * (hash_t - B * text[i-n]) + text[i]);
     if(hash_p == hash_t) index.push_back(i-n+1);
