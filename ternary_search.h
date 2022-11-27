c1ef0a6de6951966c53e04bef45ca66d2bc7e50a
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2022-11-27 15:51:55 +0900

    integer ternary_search

--- ternary_search.h
+++ ternary_search.h
@@ -1,11 +1,12 @@
-double ternary_search(double low, double high, int iteration, function<double(double)> f) {
+template<class F>
+long long ternary_search(long long low, long long high, F f) {
   /* O(iteration * O(f)) */
   /* Find the value that minimizes f. */
-  while (iteration--) {
-    double m1 = (low * 2 + high) / 3;
-    double m2 = (low + high * 2) / 3;
+  while (high-low > 2) {
+    long long m1 = low + (high-low) / 3;
+    long long m2 = high - (high-low) / 3;
     if (f(m1) < f(m2)) high = m2;
     else low = m1;
   }
-  return low;
+  return min({make_pair(f(low),low), make_pair(f(low+1),low+1), make_pair(f(low+2),low+1)}).second;
 }
---
+++ ternary_search_float.h
@@ -0,0 +1,12 @@
+template<class F>
+double ternary_search(double low, double high, int iteration, F f) {
+  /* O(iteration * O(f)) */
+  /* Find the value that minimizes f. */
+  while (iteration--) {
+    double m1 = (low * 2 + high) / 3;
+    double m2 = (low + high * 2) / 3;
+    if (f(m1) < f(m2)) high = m2;
+    else low = m1;
+  }
+  return low;
+}
