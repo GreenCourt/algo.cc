b92246e345a092111de003a59f165b736dc6242a
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2023-01-09 12:14:18 +0900

    Update rolling_hash.h

--- rolling_hash.h
+++ rolling_hash.h
@@ -1,27 +1,33 @@
 template<typename T>
-vector<int> find_all_matches_by_rolling_hash(const T& text, const T& pattern, unsigned long long b=1000000007) {
-  /* O(n + m) */
-  int n = pattern.size();
-  int m = text.size();
-  assert(n > 0);
-  assert(m > 0);
-  if(n > m) return vector<int>();
+struct RollingHash {
+  const T& text;
+  int n;
+  vector<unsigned long long> pow_b;
+  vector<unsigned long long> hash;
+  // hash( [0,r) ) = sum([text[i] * pow(b, n-i-1) for i in range(n)])
+  // hash( [l,r) ) = hash( [0,r) ) - pow(b, r - l) * hash( [0,l) )
 
-  /* For unsigned long long, overflow is same as mod 2^64 */
-  unsigned long long B = 1;
-  for(int i=0; i<n-1; ++i) B *= b;
+  RollingHash(const T& text, unsigned long long b=1000000007) : text(text), n(text.size()) {
+    /* O(n) */
+    assert(n > 0);
+    pow_b = vector<unsigned long long>(n+1, 1ULL);
+    hash.resize(n+1);
 
-  unsigned long long hash_p = 0, hash_t= 0;
-  for(int i=0; i<n; ++i) {
-    hash_p = b * hash_p + pattern[i];
-    hash_t = b * hash_t + text[i];
+    /* For unsigned long long, overflow is same as mod 2^64 */
+    pow_b[0] = 1, hash[0] = 0;
+    for(int i=1; i<=n; ++i) {
+      pow_b[i] = pow_b[i-1] * b;
+      hash[i] = b * hash[i-1] + text[i-1];
+    }
   }
-
-  vector<int> index;
-  if(hash_p == hash_t) index.push_back(0);
-  for(int i=n; i<m; i++) {
-    hash_t = (b * (hash_t - B * text[i-n]) + text[i]);
-    if(hash_p == hash_t) index.push_back(i-n+1);
+  unsigned long long get(int l, int r) { // [l,r)
+    /* O(1) */
+    assert(0 <= l && l <= r && r <= n);
+    return hash[r] - pow_b[r-l] * hash[l]; 
   }
-  return index;
-}
+  unsigned long long get(int r) { // [0,r)
+    /* O(1) */
+    assert(0 <= r && r <= n);
+    return hash[r];
+  }
+};
