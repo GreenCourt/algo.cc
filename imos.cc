1314c9c85dcd2c71920c6a2dd9cf8090bf2bd4a3
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2023-11-11 08:55:10 +0000

    update imos

--- imos.cc
+++ imos.cc
@@ -1,10 +1,13 @@
-vector<int> Imos(int mx, vector<pair<int,int>> const &area) {
-  /* O(mx + area.size()) */
-  vector<int> d(mx+1);
-  for(auto [l,r] : area) {
-    assert(0<=l && l < r && r <=mx+1); /* [l,r) */
-    d[l]++;
-    if(r <= mx) d[r]--;
+template<typename T=long long, typename S>
+vector<T> Imos(int mx, vector<int> const &left, vector<int> const &right, vector<S> const &val) {
+  /* O(mx + val.size()) */
+  int sz = val.size();
+  assert(sz == left.size() && sz == right.size());
+  vector<T> d(mx+1);
+  for(int i=0; i<sz; ++i) {
+    assert(0<=left[i] && left[i] < right[i] && right[i] <=mx+1); /* [l,r) */
+    d[left[i]] += val[i];
+    if(right[i] <= mx) d[right[i]] -= val[i];
   }
   for(int i=1; i<=mx; ++i) d[i] += d[i-1];
   return d;
--- imos2d.cc
+++ imos2d.cc
@@ -1,17 +1,24 @@
-using area = tuple<int,int,int,int>; /* x_begin, y_begin, x_end, y_end
-                                        (half-open)
-                                        x_begin <= x < x_end
-                                        y_begin <= y < y_end */
-vector<vector<int>> Imos2D(int x_max, int y_max, vector<area> const &A) {
+template<typename T=long long, typename S>
+vector<vector<T>> Imos2D(
+    int x_max, int y_max,
+    vector<int> const &x_begin, // half-open
+    vector<int> const &y_begin, // x_begin <= x < x_end
+    vector<int> const &x_end,   // y_begin <= y < y_end
+    vector<int> const &y_end,
+    vector<S> const &val
+    ) {
   /* O(x_max*y_max + A.size()) */
-  vector d(x_max+1, vector<int>(y_max+1));
-  for(auto [x_begin, y_begin, x_end, y_end] : A) {
-    assert(0 <= x_begin && x_begin <= x_end && x_end <= x_max+1);
-    assert(0 <= y_begin && y_begin <= y_end && y_end <= y_max+1);
-    d[x_begin][y_begin]++;
-    if(y_end <= y_max) d[x_begin][y_end]--;
-    if(x_end <= x_max) d[x_end][y_begin]--;
-    if(y_end <= y_max && x_end <= x_max) d[x_end][y_end]++;
+  vector d(x_max+1, vector<T>(y_max+1));
+  int sz = val.size();
+  assert(sz == x_begin.size() && sz == x_end.size());
+  assert(sz == y_begin.size() && sz == y_end.size());
+  for(int i=0; i<sz; ++i) {
+    assert(0 <= x_begin[i] && x_begin[i] <= x_end[i] && x_end[i] <= x_max+1);
+    assert(0 <= y_begin[i] && y_begin[i] <= y_end[i] && y_end[i] <= y_max+1);
+    d[x_begin[i]][y_begin[i]] += val[i];
+    if(y_end[i] <= y_max) d[x_begin[i]][y_end[i]] -= val[i];
+    if(x_end[i] <= x_max) d[x_end[i]][y_begin[i]] -= val[i];
+    if(y_end[i] <= y_max && x_end[i] <= x_max) d[x_end[i]][y_end[i]] += val[i];
   }
   for(int x=0; x<=x_max; ++x) for(int y=1; y<=y_max; ++y) d[x][y] += d[x][y-1];
   for(int y=0; y<=y_max; ++y) for(int x=1; x<=x_max; ++x) d[x][y] += d[x-1][y];
