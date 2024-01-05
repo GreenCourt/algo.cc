6cbe1058378d1b395247afaf834eb44ceaeee265
GreenCourt <45067835+GreenCourt@users.noreply.github.com>
2024-01-05 10:56:36 +0000

    add inversion_number.cc

---
+++ inversion_number.cc
@@ -0,0 +1,28 @@
+template<typename T>
+long long inversion_number(const vector<T> &a, T mn, T mx) {
+  /* O(n log n) */
+  int n = a.size();
+  T sz = mx - mn + 1;
+  assert(mn <= mx);
+  fenwick_tree<long long> ft(sz);
+  long long x = 0;
+  for(int i=0; i<n; ++i) {
+    assert(mn <= a[i] && a[i] <= mx);
+    ft.add(a[i]-mn, 1);
+    x += ft.sum(a[i]-mn+1, sz);
+  }
+  return x;
+}
+
+template<typename T>
+long long inversion_number(const vector<T> &a) {
+  /* O(n log n) */
+  int n = a.size();
+   /* coordinate compression */
+  vector<T> b(a);
+  vector<int> c(n);
+  sort(b.begin(), b.end());
+  b.erase(unique(b.begin(), b.end()), b.end());
+  for(int i=0; i<n; ++i) c[i] = lower_bound(b.begin(), b.end(), a[i]) - b.begin();
+  return inversion_number<int>(c, 0, b.size()-1);
+}
